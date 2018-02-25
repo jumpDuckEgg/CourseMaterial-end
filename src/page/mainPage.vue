baeadc24ad41b7b03af3bf644a6d60656e216450
jumpduckegg <jumpduckegg@gmail.com>
2018-02-26 00:56:07 +0800

    用户

--- src/page/mainPage.vue
+++ src/page/mainPage.vue
@@ -34,8 +34,6 @@
               <el-menu-item-group title="分组2">
                 <el-menu-item index="1-4" @click="tolink('/course/addCourseMaterial')">添加课程资料</el-menu-item>
                 <el-menu-item index="1-5" @click="tolink('/course/modifyCourseMaterial')">修改课程资料</el-menu-item>
-                <el-menu-item index="1-6" @click="tolink('/course/addOnlineTest')">添加在线测试</el-menu-item>
-                <el-menu-item index="1-7" @click="tolink('/course/modifyOnlineTest')">修改在线测试</el-menu-item>
               </el-menu-item-group>
               <el-submenu index="1-8">
                 <template slot="title">选项4</template>
@@ -46,10 +44,28 @@
               <i class="el-icon-menu"></i>
               <span slot="title">导航二</span>
             </el-menu-item>
-            <el-menu-item index="3">
-              <i class="el-icon-setting"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
+            
+            <el-submenu index="3">
+               <template slot="title">
+                <i class="el-icon-setting"></i>
+                <span>用户管理</span>
+              </template>
+               <el-menu-item-group>
+                <template slot="title">分组一</template>
+               <el-menu-item index="3-1" @click="tolink('/user')">用户信息</el-menu-item>
+              </el-menu-item-group>
+             </el-submenu>
+             <el-submenu index="4">
+               <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>在线测试</span>
+              </template>
+               <el-menu-item-group>
+                <template slot="title">分组一</template>
+               <el-menu-item index="4-1" @click="tolink('/course/addOnlineTest')">添加在线测试</el-menu-item>
+                <el-menu-item index="4-2" @click="tolink('/course/modifyOnlineTest')">修改在线测试</el-menu-item>
+              </el-menu-item-group>
+             </el-submenu>
           </el-menu>
         </el-aside>
         <el-main>
@@ -62,31 +78,31 @@
 
 <script>
 export default {
-    name: "mainPage",
-    data() {
-        return {};
+  name: "mainPage",
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      //清除token
+      this.$store.dispatch("UserLogout");
+      if (!this.$store.state.token) {
+        this.$router.push("/login");
+        this.$message({
+          type: "success",
+          message: "登出成功"
+        });
+      } else {
+        this.$message({
+          type: "info",
+          message: "登出失败"
+        });
+      }
     },
-    methods: {
-        logout() {
-            //清除token
-            this.$store.dispatch("UserLogout");
-            if (!this.$store.state.token) {
-                this.$router.push("/login");
-                this.$message({
-                    type: "success",
-                    message: "登出成功"
-                });
-            } else {
-                this.$message({
-                    type: "info",
-                    message: "登出失败"
-                });
-            }
-        },
-        tolink(data) {
-            this.$router.push(data);
-        }
+    tolink(data) {
+      this.$router.push(data);
     }
+  }
 };
 </script>
 
@@ -94,45 +110,45 @@ export default {
 <style lang='scss'scoped>
 .mainPage,
 .el-container {
-    width: 100%;
-    height: 100%;
-    min-width: 1200px;
+  width: 100%;
+  height: 100%;
+  min-width: 1200px;
 }
 .el-header {
-    border-bottom: solid 1px #e6e6e6;
+  border-bottom: solid 1px #e6e6e6;
 }
 .el-aside {
-    border-right: solid 1px #e6e6e6;
-    box-sizing: border-box;
+  border-right: solid 1px #e6e6e6;
+  box-sizing: border-box;
 }
 ul {
-    border-right: 0;
+  border-right: 0;
 }
 .header {
-    &__icon {
-        position: relative;
-        top: 4px;
-    }
-    &__title {
-        line-height: 60px;
-    }
+  &__icon {
+    position: relative;
+    top: 4px;
+  }
+  &__title {
+    line-height: 60px;
+  }
 
-    &__user {
-        text-align: right;
-        &-icon {
-            height: 46px;
-            width: 46px;
-            border-radius: 50%;
-            border: solid 2px #e6e6e6;
-            box-sizing: border-box;
-            margin: 0 10px;
-            position: relative;
-            top: 7px;
-        }
-        &-btn {
-            position: relative;
-            top: -10px;
-        }
+  &__user {
+    text-align: right;
+    &-icon {
+      height: 46px;
+      width: 46px;
+      border-radius: 50%;
+      border: solid 2px #e6e6e6;
+      box-sizing: border-box;
+      margin: 0 10px;
+      position: relative;
+      top: 7px;
     }
+    &-btn {
+      position: relative;
+      top: -10px;
+    }
+  }
 }
 </style>
--- src/page/user/user.vue
+++ src/page/user/user.vue
@@ -1,33 +1,66 @@
 <template>
   <div class="userInformation">
     <h1>{{ msg }}</h1>
-    <div>{{userInfo}}</div>
+
+    <div class="box">
+      <el-table border
+      :data="userInfo"
+      style="width: 100%">
+      <el-table-column
+        prop="user_id"
+        label="用户ID"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="username"
+        label="用户名"
+        width="180">
+      </el-table-column>
+      <el-table-column label="用户类型" width="150">
+          <template slot-scope="scope">
+              {{scope.row.userType}}
+          </template>
+        </el-table-column>
+      <el-table-column label="用户头像" width="150">
+          <template slot-scope="scope">
+            <img :src="scope.row.userImage" alt="用户头像" style="width:40px;height:40px;">
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" >
+          <template slot-scope="scope">
+            <el-button size="mini"  >更改类型</el-button>
+            <el-button size="mini" type="danger" >删除</el-button>
+          </template>
+        </el-table-column>
+    </el-table>
+    </div>
   </div>
 </template>
 
 <script>
-import api from '../../util/api.js'
+import api from "../../util/api.js";
 export default {
-  name: 'userInformation',
-  data () {
+  name: "userInformation",
+  data() {
     return {
-      msg: '我是用户信息详情',
-      userInfo:''
-    }
+      msg: "用户信息详情",
+      userInfo: []
+    };
   },
-  created(){
-      api.getAllUsers().then((data)=>{
-          let result=data.data;
-          if(result.code==6){
-              
-          }
-          this.userInfo=result;
-      })
+  created() {
+    api.getAllUsers().then(data => {
+      let result = data.data;
+      if (result.code == 6) {
+      }
+      this.userInfo = result.data;
+    });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss'scoped>
-
+.box{
+  margin: 10px 0;
+}
 </style>
